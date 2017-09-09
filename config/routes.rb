4f4b401b1c627feee262580760f5f9799f9d59d4
川口拓巳 <kawaguchitakumi@kawaguchitakuminoMacBook-Pro.local>
2017-09-09 10:55:39 +0900

    ログイン・サインイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'sessions'
-  }
+  devise_for :users
   root to: "pages#index"
-
   get 'pages/index'
   get 'pages/show'
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
