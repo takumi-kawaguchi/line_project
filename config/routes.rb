59d61ce4af789abdb965e8aeca3e25842aac8458
川口拓巳 <kawaguchitakumi@kawaguchitakuminoMacBook-Pro.local>
2017-09-10 12:40:17 +0900

    ログアウト機能のbug fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'pages/show'
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    delete '/users/sign_out' => 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
